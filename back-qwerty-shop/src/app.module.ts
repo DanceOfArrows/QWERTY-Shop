4816bccff5ca6b9d6f97cf2a747d4076097b5945
Seamus Le <seamust.le@gmail.com>
2021-04-28 18:15:53 -0700

    Reverted gitignore changes

--- back-qwerty-shop/src/app.module.ts
+++ back-qwerty-shop/src/app.module.ts
@@ -18,7 +18,7 @@ import { join } from 'path';
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://qwerty-shop-app.herokuapp.com',
         credentials: true,
       },
       sortSchema: true,
